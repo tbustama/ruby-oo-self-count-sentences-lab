dd22b05b74b1415380f6f63d63e4b7bf38c6ab98
Taylor Bustamante <taylorbustamante16@gmail.com>
2020-11-18 14:10:45 -0800

    Changed string class

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,32 @@ require 'pry'
 class String
 
   def sentence?
-    
+    if self[-1] == "."
+      return true
+    else 
+      return false
+    end
   end
 
   def question?
-
+    if self[-1] == "?"
+      return true
+    else 
+      return false
+    end
   end
 
   def exclamation?
-
+    if self[-1] == "!"
+      return true
+    else 
+      return false
+    end
   end
 
   def count_sentences
-
+    self.scan(/[^\.!?]+[\.!?]/).map(&:strip).count
+    
   end
 end
+
